b99f4e9c56e375d41b117a6f83dd88ca15abae8a
andayaaaron <andayaaaron@gmail.com>
2021-11-10 22:49:34 -0800

    Update PlayerInput.cs

--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -16,7 +16,7 @@ public class PlayerInput : MonoBehaviour
         cController = GetComponent<CharacterController>();
     }
 
-
+    //smeg
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
