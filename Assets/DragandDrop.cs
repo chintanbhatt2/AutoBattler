fff3ed87d5f6852f836cb769e1812e6b28954d81
andayaaaron <andayaaaron@gmail.com>
2021-11-14 12:59:37 -0800

    Drag and Drop
    drag and drop stuff with main gameboard

---
+++ Assets/DragandDrop.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragandDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    public void OnBeginDrag(PointerEventData eventData){
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData){
+        Debug.Log("OnDrag");
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData){
+        Debug.Log("OnEndDrag");
+    }
+}
